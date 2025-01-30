bc24abac423a67e74a658b9af2ea105ffc3665f2
RahulThakur014 <rahulj.thakur.26@gmail.com>
2025-01-30 22:27:35 +0530

    Update patternsquations.java

--- patternsquations.java
+++ patternsquations.java
@@ -1,33 +1,33 @@
 public class patternsquations {
 
     public static void main(String[] args) {
-        // // Solid Pattern (row=4, colomn=5)
+        //1. Solid Pattern (row=4, colomn=5)
 
-        // for (int i = 1; i <= 4; i++){ //  outer loop(rows)
-                // inner loop(colums)
-        //     for(int j = 1; j <= 5; j++){
-        //         System.out.print("*"); // not use println
-        //     }
-        //     System.out.println("*");
-        // }
-
-
-        // Hollow box pattern
-
-        // for(int i = 1; i <= 4; i++) { // inner loop(rows)
-        //     //  inner loop(colums)
-        //     for(int j = 1; j <= 5; j++){
-        //         if(i == 1 || j == 1 || i == 4 || j == 5){
-        //             System.out.print("*");
-        //         } else{
-        //             System.out.print(" ");            
-        //         }
-        //     } System.out.println();
-        // } 
+        for (int i = 1; i <= 4; i++){ //  outer loop(rows)
+                inner loop(colums)
+            for(int j = 1; j <= 5; j++){
+                System.out.print("*"); // not use println
+            }
+            System.out.println("*");
+        }
 
 
+        //2. Hollow box pattern
 
-        // Half piramid
+        for(int i = 1; i <= 4; i++) { // inner loop(rows)
+            //  inner loop(colums)
+            for(int j = 1; j <= 5; j++){
+                if(i == 1 || j == 1 || i == 4 || j == 5){
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");            
+                }
+            } System.out.println();
+        } 
+
+
+
+        //3. Half pyramid
 
         for (int i = 1; i <= 4; i++){ //  outer loop(rows)
             // inner loop(colums)
