f8dc1c8b52f508ffa8015e7ba92dc4e335ab1e10
RahulThakur014 <rahulj.thakur.26@gmail.com>
2025-01-31 14:28:15 +0530

    adding Half pyramid with number code

--- patternsquations.java
+++ patternsquations.java
@@ -64,6 +64,17 @@ public class patternsquations {
 
             System.out.println();
         } 
+
+        // 6. Half pyramid with number
+        int n = 5;
+
+        for (int i = 1; i <= n; i++){ //  outer loop(rows)
+            // inner loop(colums)
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
         
     }
 
